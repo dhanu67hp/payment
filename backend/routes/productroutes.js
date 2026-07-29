167182237ac336bc37874c50b1ba78d2883d0bab
dhanu67hp <puttagangadharappahs@gmail.com>
2026-07-29 22:35:55 +0530

    Delete backend directory

--- backend/routes/productroutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getkey,paymentVerification ,processpayment } from "../controller/productcontroller.js";
-const router = express.Router();
-
-router.post("/payment/process", processpayment);
-router.route("/getkey").get(getkey);
-router.route("/paymentVerification").post(paymentVerification);
-export default router;
